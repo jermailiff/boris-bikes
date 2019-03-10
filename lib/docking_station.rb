740fd12ad87c7d2f0e7cd87a75b4855a08ab007b
JermaineRaphael <jermaine.ijieh@fanfair.co>
2019-03-10 16:44:42 +0000

    11/03/19 part13

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,17 @@
 require_relative 'bike'
 
 class DockingStation
-    def release_bike
-        Bike.new
+    
+    attr_reader :bike
+
+    def dock(bike)
+        fail 'Docking station full' if @bike
+        @bike = bike
     end
+
+    def release_bike
+        fail 'No bikes available' unless @bike
+        @bike
+    end
+    
 end
