4645a80e9dbb2dc52c1a7b08a483c564e2b07f1c
Jermaine <jijieh@gmail.com>
2019-04-13 11:52:21 +0100

    updated garage fix testing

--- lib/garage.rb
+++ lib/garage.rb
@@ -8,7 +8,6 @@ class Garage
 
     def fix 
         @bikes.select {|bike|bike.broken == true}.each do |bike| 
-            p bike.broken
             bike.broken = false
         end
     end
--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -2,16 +2,15 @@ require 'garage'
 
 describe 'Garage' do
     subject {Garage.new}
-    let(:bike) { double('bike', 'broken=' => false, 'broken' => true) }
+    let(:bike) { double('bike', 'broken' => [true, false]) }
 
     it {is_expected.to respond_to(:fix)}
 
     it 'fixes broken bikes' do
         subject.bikes << bike
-        p subject.bikes
+        subject.bikes
         subject.fix
-        bike1 = subject.bikes[0]
-        expect(bike1.broken).to be_falsey
+        expect{subject.bikes.to change('bike', 'broken'.from(true).to(false))}
     end
 
 
